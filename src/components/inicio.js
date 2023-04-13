45ad8126b0d8b20863054f5b0ed6ac1f5705afa8
Davidson George <daviiiwilliams@live.com>
2023-04-13 16:09:31 -0300

    update 1.00.13

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -2,30 +2,31 @@ import React from "react";
 import Cardapio from "./Cardapio";
 import dados from '../configure.json';
 
+
 function Inicio() {
     // const [palavra1, palavra2] = dados.dados.titulo.split(' & ');
 
     return (
         <>
-           {/* */} 
-
-            <div  className="principal-demas">
+            <div className="principal-demas">
                 <div className="imagem-principal" >
-                    
+
 
                 </div>
                 <div id="conteudoPagina"></div>
                 <div className="principal-demas-titulo">
-                        <h1>{/*palavra1*/} <img className='logoheader' src='https://dagesico.pythonanywhere.com/static/img/demas/logodemas.webp' alt='' /> {/*palavra2*/}</h1>
-                        <h2>{dados.dados.stitulo}</h2>
-                        <div className="principal-demas-link">
-                            <a href="#conteudoPagina">Entrar</a>
-                        </div>
-                 
+                    <h1>{/*palavra1*/} <img className='logoheader' src='https://dagesico.pythonanywhere.com/static/img/demas/logodemas.webp' alt='' /> {/*palavra2*/}</h1>
+                    <h2>{dados.dados.stitulo}</h2>
+                    <div className="principal-demas-link">
+                        <a href="#conteudoPagina">Entrar</a>
+
                     </div>
-                 
-                <div  className='divisao' />
-          
+                    <button id="install-button" hidden>Instalar App</button>
+
+                </div>
+
+                <div className='divisao' />
+
                 <div className="conteudo-principal">
                     <div className="conteudo-principal-demas">
                         {<Cardapio />}
--- src/index.js
+++ src/index.js
@@ -4,9 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
+let deferredPrompt;
+
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  // Update UI notify the user they can add to home screen
+  showInstallPromotion();
+});
+
+function showInstallPromotion() {
+  const installButton = document.getElementById('install-button');
+  installButton.hidden = false;
+  installButton.addEventListener('click', (e) => {
+    // Hide the app provided install promotion
+    installButton.hidden = true;
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
   });
 }
 
