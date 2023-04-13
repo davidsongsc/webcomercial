9b3c25a7e8efb83972cce824f5c179d2f1d8da9c
Davidson George <daviiiwilliams@live.com>
2023-04-13 15:52:26 -0300

    update 1.00.12

--- workbox-config.js
+++ workbox-config.js
@@ -1,10 +1,35 @@
 module.exports = {
-    globDirectory: './',
-    globPatterns: ['**/*.{html,css,js}'],
-    swDest: './sw.js',
-    runtimeCaching: [{
-      urlPattern: /https:\/\/jsonplaceholder\.typicode\.com\/todos/,
-      handler: 'StaleWhileRevalidate'
-    }]
-  };
-  
+  globDirectory: 'build/',
+  globPatterns: ['**/*.{html,js,css}'],
+  swDest: 'build/service-worker.js',
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 20,
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'google-fonts-stylesheets',
+      },
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-webfonts',
+        expiration: {
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+          maxEntries: 30,
+        },
+      },
+    },
+  ],
+};
