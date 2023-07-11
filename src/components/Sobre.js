35598ae5b2be2bfcd52272693640350a165ae826
Davidson George <daviiiwilliams@live.com>
2023-07-11 12:37:44 -0300

    Update Sobre.js

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -7,8 +7,8 @@ function Sobre({api}) {
     <>
     <BaseLogo/>
     <div id='sobre'>
-      <h1>CONHECENDO O DEMAS & DIVAS</h1>
-      <p>O Demas & Divas é uma steak house que se iniciou com o famoso sanduíche de pernil do Sr.Demas, que era um sucesso nas festas de família. Assim, surgiu a ideia de criar um local aconchegante para reunir os amigos, onde as famílias pudessem provar os sanduíches e outros pratos americanos. As Divas e astros do cinema foram a fonte de inspiração para criar o ambiente. A sra Diva é uma doceira de mão cheia, e cria as mais deliciosas e irresistíveis sobremesas, que são as estrelas da casa. Com o princípio de oferecer sempre produtos de qualidade, o Demas e Divas utiliza temperos naturais e fabrica seus pratos de forma artesanal, como os hamburgueres, brownies, quiches e pães. A nossa unidade em Vila Valqueire no ano de 2008, hoje com 14 anos oferecendo o melhor para seus clientes. Venha conhecer uma uma casa totalmente inovadora e hollywoodiana. </p>
+      <h1>CONHECENDO A LOJA</h1>
+      <p>O DG Meat Club é uma steak house que se iniciou com o famoso sanduíche de pernil do Sr.Demas, que era um sucesso nas festas de família. Assim, surgiu a ideia de criar um local aconchegante para reunir os amigos, onde as famílias pudessem provar os sanduíches e outros pratos americanos. As Divas e astros do cinema foram a fonte de inspiração para criar o ambiente. A sra Diva é uma doceira de mão cheia, e cria as mais deliciosas e irresistíveis sobremesas, que são as estrelas da casa. Com o princípio de oferecer sempre produtos de qualidade, o Demas e Divas utiliza temperos naturais e fabrica seus pratos de forma artesanal, como os hamburgueres, brownies, quiches e pães. A nossa unidade em Vila Valqueire no ano de 2008, hoje com 14 anos oferecendo o melhor para seus clientes. Venha conhecer uma uma casa totalmente inovadora e hollywoodiana. </p>
     </div>
     <Footer></Footer>
     </>
